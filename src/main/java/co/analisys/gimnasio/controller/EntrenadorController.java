10b9004dc89c5463bbbb67a5af51bc411396075a
Daniel Jose <danieljpc19@gmail.com>
2026-08-24 15:26:20 -0500

    feat(entrenadores): create controller
    create entrenadores controller by renamings gimnasio controller

--- src/main/java/co/analisys/gimnasio/controller/EntrenadorController.java
+++ src/main/java/co/analisys/gimnasio/controller/EntrenadorController.java
@@ -1,8 +1,6 @@
 package co.analisys.gimnasio.controller;
 
 import co.analisys.gimnasio.model.Entrenador;
-import co.analisys.gimnasio.model.Equipo;
-import co.analisys.gimnasio.model.Miembro;
 import co.analisys.gimnasio.service.GimnasioService;
 import lombok.RequiredArgsConstructor;
 
@@ -11,29 +9,19 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/gimnasio")
+@RequestMapping("/api/gimnasio/entrenadores")
 @RequiredArgsConstructor
-public class GimnasioController {
+public class EntrenadorController {
     // TODO: Decoupled into multiple controllers to separate logic business.
     private final GimnasioService gimnasioService;
 
-    @PostMapping("/entrenadores")
+    @PostMapping("")
     public Entrenador agregarEntrenador(@RequestBody Entrenador entrenador) {
         return gimnasioService.agregarEntrenador(entrenador);
     }
 
-    @PostMapping("/equipos")
-    public Equipo agregarEquipo(@RequestBody Equipo equipo) {
-        return gimnasioService.agregarEquipo(equipo);
-    }
-
-    @GetMapping("/entrenadores")
+    @GetMapping("")
     public List<Entrenador> obtenerTodosEntrenadores() {
         return gimnasioService.obtenerTodosEntrenadores();
-    }
-
-    @GetMapping("/equipos")
-    public List<Equipo> obtenerTodosEquipos() {
-        return gimnasioService.obtenerTodosEquipos();
     }
 }
